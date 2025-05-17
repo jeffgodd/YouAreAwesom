75e3d32693a9316dd7a4128a7c576df38928110d
jeffgodd <jeff@leggierosw.com>
2025-05-17 15:50:19 -0700

    Initial Commit

---
+++ YouAreAwesom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YouAreAwesom
+//
+//  Created by Jeff Goddard on 5/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouAreAwesom/YouAreAwesomApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomApp.swift
+//  YouAreAwesom
+//
+//  Created by Jeff Goddard on 5/17/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
