b6dde466313095c88e1f917fe1cf12de8bc53425
jeffgodd <jeff@leggierosw.com>
2025-05-20 15:24:57 -0700

    used arrays and transitions

--- YouAreAwesom/ContentView.swift
+++ YouAreAwesom/ContentView.swift
@@ -15,30 +15,32 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
-            Spacer()
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+                .animation(.easeInOut(duration: 0.15), value: message )
             
             Image(imageName)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
-             
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-                .multilineTextAlignment(.center)
-            
+                .animation(.default, value: imageName )
+           
             Spacer()
             
             Button("Show Message") {
                 let messages = ["You Are Awesome!",
+                                "When the Genius Bar Needs Help, They Call You!",
                                 "You Are Great!",
                                 "You Are Fabulous",
                                 "You Are Amazing!",
                                 "Wonderful, That's you!",
-                                "You Are Handsome!",
-                                "When the Genius Bar Needs Help, They Call You!"]
+                                "You Are Handsome!"]
                
                 message = messages[imageNumber]
                 
