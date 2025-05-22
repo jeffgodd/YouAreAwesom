01d6451116fd01c9a448aa7597f1b7ccf3930ba0
jeffgodd <jeff@leggierosw.com>
2025-05-22 16:10:45 -0700

    Sounds, Guard-let, Errors and Magic Number avoidance +

--- YouAreAwesom/ContentView.swift
+++ YouAreAwesom/ContentView.swift
@@ -6,12 +6,17 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
     @State private var lastMessageNumber = -1 //make sure you can access 0
     @State private var lastImageNumber = -1
+    @State private var lastSoundNumber = -1
+    @State private var audioPlayer: AVAudioPlayer!
+    let numberOfImages = 10
+    let numberOfSounds = 6
     
     var body: some View {
         
@@ -24,6 +29,8 @@ struct ContentView: View {
                 .minimumScaleFactor(0.5)
                 .frame(height: 100)
                 .animation(.easeInOut(duration: 0.15), value: message )
+            
+            Spacer()
             
             Image(imageName)
                 .resizable()
@@ -44,8 +51,6 @@ struct ContentView: View {
                                 "You Are Handsome!"]
                 
                 var messageNumber: Int
-                var imageNumber: Int
-                
                 repeat {
                     messageNumber = Int.random(in: 0...messages.count-1)
                 } while messageNumber == lastMessageNumber
@@ -53,12 +58,32 @@ struct ContentView: View {
                 message = messages[messageNumber]
                 lastMessageNumber = messageNumber
                 
+                var imageNumber: Int
                 repeat {
-                    imageNumber = Int.random(in: 0...9)
+                    imageNumber = Int.random(in: 0...(numberOfImages-1))
                 } while imageNumber == lastImageNumber
                 
                 lastImageNumber = imageNumber
                 imageName = "image\(imageNumber)"
+                
+                var soundNumber: Int
+                repeat {
+                    soundNumber = Int.random(in: 0..<numberOfSounds)  //made the "upto" type of range
+                } while soundNumber == lastSoundNumber
+                
+                lastSoundNumber = soundNumber
+                let soundName = "sound\(soundNumber)"
+              
+                guard let soundFile = NSDataAsset(name: soundName) else {
+                    print("😡 Could not read file named \(soundName)")
+                    return
+                }
+                do {
+                    audioPlayer = try AVAudioPlayer(data: soundFile.data)
+                    audioPlayer.play()
+                } catch {
+                    print("😡 ERROR: \(error.localizedDescription) creating AVAudioPlayer")
+                }
                 
             }
             .buttonStyle(.borderedProminent)
